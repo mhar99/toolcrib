a6fcdf006a6a4b64070f906ae992036fffe1bc3f
mhar99 <105839988+mhar99@users.noreply.github.com>
2022-10-25 16:55:26 +0700

    tabel-meminjam

--- tabel/js/app.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() { // Handler for .ready() called.
-    // sebagai action ketika row diklik
-    $('.header').click(function(){
-         $(this).toggleClass('expand').nextUntil('tr.header').slideToggle(100);
-    });
-  
-    $('.header').click();
-  });
